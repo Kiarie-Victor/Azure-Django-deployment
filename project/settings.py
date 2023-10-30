cb42e56271f9982f7716e01e74c2c5e486bfae4b
dreytted <victorkiarie8@gmail.com>
2023-10-31 02:55:11 +0300

    added the msal template files

--- project/settings.py
+++ project/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dog_shelters.context_processors.custom_context'
             ],
         },
     },
