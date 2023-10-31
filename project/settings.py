84a24706695f53a41e56d90ba340f94ef73ac4fd
dreytted <victorkiarie8@gmail.com>
2023-10-31 03:14:38 +0300

    really hadnt seen this bug

--- project/settings.py
+++ project/settings.py
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'dog_shelters.context_processors.custom_context'
+                'dog_shelters.context_processors.context'
             ],
         },
     },
